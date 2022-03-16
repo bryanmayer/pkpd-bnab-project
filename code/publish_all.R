a7e6103330df3c0ebb08591a23458ec188b46116
Bryan <bmayer@fredhutch.org>
2022-03-15 18:12:28 -0700

    final background materials for paper

--- code/publish_all.R
+++ code/publish_all.R
@@ -1,4 +1,10 @@
 # publishes full pipeline
+
+if(!exists("git_msg")){
+  print("set a commit message in git_msg")
+  stop("no commit message")
+}
+
 wflow_publish(
   c(
     "analysis/_site.yml",
@@ -9,5 +15,7 @@ wflow_publish(
     "analysis/empirical-case-study.Rmd",
     "analysis/hill-slope-meta.Rmd",
     "analysis/titer-analysis.Rmd"
-  )
+  ),
+  message = git_msg
 )
+rm(git_msg)
